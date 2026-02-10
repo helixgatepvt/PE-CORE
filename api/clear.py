eaced865e9a641168b3e7c55336f9371539cae45
HelixGate <helixgateprivate@gmail.com>
2026-02-10 22:58:37 +0530

    Update clear.py

--- api/clear.py
+++ api/clear.py
@@ -1,1 +1,16 @@
+from engine.ccm.clearance_engine import generate_clearance
+from engine.ccm.fee_record import calculate_ccm_fee
+
+def clear_capital(deal_id, snapshot, deployed_capital):
+    clearance = generate_clearance(snapshot, deployed_capital)
+    fee = calculate_ccm_fee(deployed_capital)
+
+    # persist clearance + fee in DB (pseudo)
+    save_clearance(clearance)
+    save_fee(fee)
+
+    return {
+        "clearance": clearance,
+        "fee": fee
+    }
 
