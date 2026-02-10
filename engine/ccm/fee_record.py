6eb3f242d84a15cd74ea07ebce77853802fdaab2
HelixGate <helixgateprivate@gmail.com>
2026-02-10 22:58:15 +0530

    Update fee_record.py

--- engine/ccm/fee_record.py
+++ engine/ccm/fee_record.py
@@ -1,1 +1,8 @@
+def calculate_ccm_fee(deployed_capital, rate=0.02):
+    return {
+        "deployed_capital_amount": deployed_capital,
+        "fee_rate": rate,
+        "calculated_fee": deployed_capital * rate,
+        "fee_status": "RECORDED"
+    }
 
